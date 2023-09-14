d8b345195996c99dacfd25d13e6c22d92590e410
Paul <pgroenewoud@1stquad.com>
2023-09-14 21:01:25 +0200

    Added query element and reset prompt immediately

--- public/app.js
+++ public/app.js
@@ -1,5 +1,8 @@
 const form = document.querySelector("form");
+
 const messageInput = document.getElementById("message");
+
+const queryEl = document.getElementById("query");
 const responseEl = document.getElementById("response");
 const messageBtn = document.getElementById("message-btn");
 
@@ -7,6 +10,10 @@ form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   console.log(messageInput.value);
+  const queryText = messageInput.value;
+  messageInput.value = "";
+  queryEl.innerHTML = queryText;
+ 
 
   messageBtn.disabled = true;
   messageBtn.innerHTML = "Pending...";
@@ -17,17 +24,20 @@ form.addEventListener("submit", async (e) => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ message: messageInput.value }),
+      body: JSON.stringify({ message: queryText }),
     });
 
     const data = await res.json();
-
     responseEl.innerHTML = data.message;
+
   } catch (error) {
+
+    queryEl.innerHTML = `Failed request: ${queryText}`;
     responseEl.innerHTML = error.message;
+
   } finally {
     messageBtn.disabled = false;
     messageBtn.innerHTML = "Send";
-    messageInput.value = "";
+    //messageInput.value = "";
   }
 });
